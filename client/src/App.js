2dee6c9493535d62483fcd2db1374cff94585c95
Rafael Cintra <ra.cintra@gmail.com>
2022-06-28 15:03:06 +0100

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Client React</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>Client React</h1>
+      </div>
+    </>
   );
 }
 
